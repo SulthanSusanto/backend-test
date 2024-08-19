4957d605b2d39d330226d7151938ff65cd4843d6
sulthan susanto <sulthanltsusanto@gmail.com>
2024-08-19 23:10:25 +0800

    get the data

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+import Lead from './source/models/Lead';
+
 export default (express) => {
   const app = express();
 
   app.use(express.json());
 
-  app.get('/test', (req, res) => {
-    return res.json({
-      res: 'Hello, world!',
-    });
+  app.get('/', (req, res) => {
+    res.send('welcome to the world');
+  });
+
+  app.get('/test', async (req, res) => {
+    try {
+      const leadData = await Lead.findAll();
+      res.json({ data: leadData });
+    } catch (err) {
+      res.json({ message: 'error to get the data' });
+    }
   });
   const PORT = process.env.PORT || 3000;
 
