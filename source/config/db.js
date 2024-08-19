ca3241605bccaf8428caec5b31a3fbfde02feb61
sulthan susanto <sulthanltsusanto@gmail.com>
2024-08-20 00:24:14 +0800

    fix database

--- source/config/db.js
+++ source/config/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import mysql2 from 'mysql2';
 
 dotenv.config();
 
@@ -8,8 +9,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
     dialect: 'mysql',
+    dialectModule: mysql2,
+    host: process.env.DB_HOST,
     logging: false, // Disable logging SQL queries to console
   },
 );
